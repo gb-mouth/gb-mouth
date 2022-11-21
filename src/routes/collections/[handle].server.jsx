9ab4188fe115fb1a65b0064976fc762e5db5f09d
tsunpoko <oor.tnpk3518@gmail.com>
2022-11-21 23:21:16 +0900

    show collection title on collection page

--- src/routes/collections/[handle].server.jsx
+++ src/routes/collections/[handle].server.jsx
@@ -31,13 +31,12 @@ export default function Collection() {
       resourceId: collection.id,
     },
   });
-
   return (
     <Layout>
       <Suspense>
         <Seo type="collection" data={collection} />
       </Suspense>
-      <PageHeader heading="Collections" variant="allCollections" />
+      <PageHeader heading={collection.title} variant="allCollections" />
       <section className="w-full gap-4 md:gap-8 grid p-6 md:p-8 lg:p-12">
         <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {collection.products.nodes.map((product) => (

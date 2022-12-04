1ca2625a04ee4486d88f13ef7e19b18ce528cfaf
tsunpoko <oor.tnpk3518@gmail.com>
2022-12-05 00:14:43 +0900

    implemented category feature

---
+++ src/routes/categories/[handle].server.jsx
@@ -0,0 +1,97 @@
+import {
+  gql,
+  useShopQuery,
+  Seo,
+  useServerAnalytics,
+  useRouteParams,
+  ShopifyAnalyticsConstants,
+} from "@shopify/hydrogen";
+import { Suspense } from "react";
+
+import { Layout } from "../../components/Layout.server";
+import ProductCard from "../../components/product/ProductCard.server";
+import { PageHeader } from "../../components/global/PageHeader";
+import { Section } from "../../components/elements/Section";
+
+export default function Collection() {
+  const { handle } = useRouteParams();
+
+  const {
+    data: { collection },
+  } = useShopQuery({
+    query: QUERY,
+    variables: {
+      handle,
+    },
+  });
+
+  useServerAnalytics({
+    shopify: {
+      pageType: ShopifyAnalyticsConstants.pageType.collection,
+      resourceId: collection.id,
+    },
+  });
+  return (
+    <Layout>
+      <Suspense>
+        <Seo type="collection" data={collection} />
+      </Suspense>
+      <PageHeader heading={collection.title} variant="allCollections" />
+      <section className="w-full gap-4 md:gap-8 grid p-6 md:p-8 lg:p-12">
+        <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {collection.products.nodes.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
+    </Layout>
+  );
+}
+
+const QUERY = gql`
+  query CollectionDetails($handle: String!) {
+    collection(handle: $handle) {
+      id
+      title
+      description
+      seo {
+        description
+        title
+      }
+      image {
+        id
+        url
+        width
+        height
+        altText
+      }
+      products(first: 8) {
+        nodes {
+          id
+          title
+          publishedAt
+          handle
+          variants(first: 1) {
+            nodes {
+              id
+              image {
+                url
+                altText
+                width
+                height
+              }
+              priceV2 {
+                amount
+                currencyCode
+              }
+              compareAtPriceV2 {
+                amount
+                currencyCode
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
--- src/routes/products/index.server.jsx
+++ src/routes/products/index.server.jsx
@@ -1,4 +1,4 @@
-import { useShopQuery, Seo, gql, useLocalization } from "@shopify/hydrogen";
+import { useShopQuery, Seo, gql, useLocalization, useUrl } from "@shopify/hydrogen";
 import { Layout } from "../../components/Layout.server";
 import { PRODUCT_CARD_FRAGMENT } from "../../lib/fragments";
 import { PAGINATION_SIZE } from "../../lib/const";
@@ -6,12 +6,23 @@ import { PageHeader } from "../../components/global/PageHeader";
 import { Section } from "../../components/elements/Section";
 import { Suspense } from "react";
 import { ProductGrid } from "../../components/product/ProductGrid.client";
+import { PullDownButton } from "../../components/global/PullDownButton.client";
 
 export default function AllProducts() {
+  const { data: c } = useShopQuery({
+    query: GET_CETEGORIES_QUERY,
+    preload: true,
+  });
+  const category_data = c.productTypes.edges
+  const categories = category_data.map(cateogry => {
+    return cateogry.node
+  });
+
   return (
     <Layout>
       <Seo type="page" data={{title: 'All Products'}} />
       <PageHeader heading="All Products" variant="allCollections" />
+      <PullDownButton categories={categories}></PullDownButton>
       <Section>
         <Suspense>
           <AllProductsGrid />
@@ -27,12 +38,20 @@ function AllProductsGrid() {
     country: {isoCode: countryCode},
   } = useLocalization();
 
+  let category = useUrl().searchParams.get("category");
+  if (!category) {
+    category = `product_type:*`
+  } else {
+    category = `product_type:${category}`
+  }
+
   const {data} = useShopQuery({
     query: ALL_PRODUCTS_QUERY,
     variables: {
       country: countryCode,
       language: languageCode,
       pageBy: PAGINATION_SIZE,
+      category: category
     },
     preload: true,
   });
@@ -42,7 +61,7 @@ function AllProductsGrid() {
   return (
     <ProductGrid
       key="products"
-      url={`/products?country=${countryCode}`}
+      url={`/products?country=${countryCode}&category=${category}`}
       collection={{products}}
     />
   );
@@ -61,6 +80,7 @@ export async function api(request, {params, queryShop}) {
   const url = new URL(request.url);
   const cursor = url.searchParams.get('cursor');
   const country = url.searchParams.get('country');
+  const category = url.searchParams.get('category');
   const {handle} = params;
 
   return await queryShop({
@@ -70,6 +90,7 @@ export async function api(request, {params, queryShop}) {
       cursor,
       pageBy: PAGINATION_SIZE,
       country,
+      category: category
     },
   });
 }
@@ -81,8 +102,9 @@ const ALL_PRODUCTS_QUERY = gql`
     $language: LanguageCode
     $pageBy: Int!
     $cursor: String
+    $category: String
   ) @inContext(country: $country, language: $language) {
-    products(first: $pageBy, after: $cursor) {
+    products(first: $pageBy, query: $category, after: $cursor) {
       nodes {
         ...ProductCard
       }
@@ -102,14 +124,25 @@ const PAGINATE_ALL_PRODUCTS_QUERY = gql`
     $cursor: String
     $country: CountryCode
     $language: LanguageCode
+    $category: String
   ) @inContext(country: $country, language: $language) {
-    products(first: $pageBy, after: $cursor) {
+    products(first: $pageBy, query: $category, after: $cursor) {
       nodes {
         ...ProductCard
       }
       pageInfo {
         hasNextPage
         endCursor
+      }
+    }
+  }
+`;
+
+const GET_CETEGORIES_QUERY = gql`
+  query {
+    productTypes(first:100) {
+      edges {
+        node
       }
     }
   }

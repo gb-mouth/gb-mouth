25069303ae61d0cad5a6ad9a82e39bb3fabca503
tsunpoko <oor.tnpk3518@gmail.com>
2022-12-12 22:38:56 +0900

    change title to `all`

--- src/components/global/Header.client.jsx
+++ src/components/global/Header.client.jsx
@@ -33,8 +33,11 @@ export default function Header({ shop }) {
       },
       {
         id: 2,
+        // `to` key の value を collection `all` に向ける
+        // collection `all` へのリンク:
+        // https://gb-mouth.myshopify.com/admin/collections/291237068989
         to: '/products',
-        title: 'Products',
+        title: 'all',
         target: '_self'
       },
     ]

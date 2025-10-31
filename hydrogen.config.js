b41d3098f49a2da62e97b1e8cda7c9926ee4235c
tsunpoko <oor.tnpk3518@gmail.com>
2025-10-31 18:40:53 +0900

    fix

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,8 +2,8 @@ import {defineConfig} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: {
-    storeDomain: process.env.SHOPIFY_STORE_DOMAIN,
-    storefrontToken: process.env.SHOPIFY_STOREFRONT_TOKEN,
-    storefrontApiVersion: process.env.SHOPIFY_STOREFRONT_API_VERSION,
+    storeDomain: process.env.SHOPIFY_STORE_DOMAIN || 'gb-mouth.myshopify.com',
+    storefrontToken: process.env.SHOPIFY_STOREFRONT_TOKEN || '43f698e85ff68a13442c77704c829138',
+    storefrontApiVersion: process.env.SHOPIFY_STOREFRONT_API_VERSION || '2022-07',
   },
 });
